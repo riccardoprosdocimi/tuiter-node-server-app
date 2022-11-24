23787d5dabc4fb8bb288c8ac855d38ebb413e40b
Riccardo Prosdocimi <riccardo.prosdocimi@gmail.com>
2022-11-24 13:19:39 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
